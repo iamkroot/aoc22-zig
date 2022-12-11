02ddfc0eb2b3ad9e85345469497de8dbc29cb198
iamkroot <kroot.patel@gmail.com>
2022-12-11 15:21:45 +0530

    day2: Finish part2

--- src/day2.zig
+++ src/day2.zig
@@ -7,7 +7,7 @@ const Outcome = enum(u8) {
     lose = 0,
 };
 
-const Play = enum(u8) { rock = 1, paper = 2, scissors = 3};
+const Play = enum(u8) { rock = 1, paper = 2, scissors = 3 };
 
 const Iteration = struct {
     their: Play,
@@ -31,6 +31,29 @@ const Iteration = struct {
             },
         };
     }
+    fn score(self: Iteration) u64 {
+        return @enumToInt(self.our) + @enumToInt(self.check());
+    }
+    fn from_outcome(their: Play, outcome: Outcome) Iteration {
+        const our = switch (their) {
+            .rock => switch (outcome) {
+                Outcome.draw => Play.rock,
+                Outcome.win => Play.paper,
+                Outcome.lose => Play.scissors,
+            },
+            .paper => switch (outcome) {
+                Outcome.lose => Play.rock,
+                Outcome.draw => Play.paper,
+                Outcome.win => Play.scissors,
+            },
+            .scissors => switch (outcome) {
+                Outcome.win => Play.rock,
+                Outcome.lose => Play.paper,
+                Outcome.draw => Play.scissors,
+            },
+        };
+        return .{ .their = their, .our = our };
+    }
 };
 
 const GameIter = struct {
@@ -45,7 +68,7 @@ const GameIter = struct {
         };
     }
 
-    fn next(self: *GameIter) ?Iteration {
+    fn next(self: *GameIter, ispart2: bool) ?Iteration {
         if (self.gameiter.next()) |line| {
             const opp = switch (line[0]) {
                 'A' => Play.rock,
@@ -53,13 +76,23 @@ const GameIter = struct {
                 'C' => Play.scissors,
                 else => unreachable,
             };
-            const our = switch (line[2]) {
-                'X' => Play.rock,
-                'Y' => Play.paper,
-                'Z' => Play.scissors,
-                else => unreachable,
-            };
-            return .{ .their = opp, .our = our };
+            if (ispart2) {
+                const outcome = switch (line[2]) {
+                    'X' => Outcome.lose,
+                    'Y' => Outcome.draw,
+                    'Z' => Outcome.win,
+                    else => unreachable,
+                };
+                return Iteration.from_outcome(opp, outcome);
+            } else {
+                const our = switch (line[2]) {
+                    'X' => Play.rock,
+                    'Y' => Play.paper,
+                    'Z' => Play.scissors,
+                    else => unreachable,
+                };
+                return .{ .their = opp, .our = our };
+            }
         } else {
             return null;
         }
@@ -73,10 +106,21 @@ pub fn part1(dataDir: std.fs.Dir) !void {
     const input = try read_input(dataDir, allocator, "day2.txt");
     var iter = GameIter.init(input);
     var total: u64 = 0;
-    while (iter.next()) |game| {
-        const outcome = game.check();
-        var score = @enumToInt(game.our) + @enumToInt(outcome);
-        total += score;
+    while (iter.next(false)) |game| {
+        total += game.score();
     }
-    std.debug.print("total: {}\n", .{ total });
+    std.debug.print("total: {}\n", .{total});
+}
+
+pub fn part2(dataDir: std.fs.Dir) !void {
+    var buffer: [10000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    const input = try read_input(dataDir, allocator, "day2.txt");
+    var iter = GameIter.init(input);
+    var total: u64 = 0;
+    while (iter.next(true)) |game| {
+        total += game.score();
+    }
+    std.debug.print("total: {}\n", .{total});
 }
--- src/main.zig
+++ src/main.zig
@@ -15,5 +15,5 @@ pub fn main() !void {
     const dataDirPath = try std.fs.realpathAlloc(allocator, rootPath("/data"));
     const dataDir = try std.fs.openDirAbsolute(dataDirPath, .{});
     try day2.part1(dataDir);
-    // try day1.part2(dataDir);
+    try day2.part2(dataDir);
 }
