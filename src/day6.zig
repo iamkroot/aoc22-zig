e8ade11dc76876728553417973b4f0cd5b77d81a
iamkroot <kroot.patel@gmail.com>
2022-12-14 23:11:09 +0530

    day6: Part 2

--- src/day6.zig
+++ src/day6.zig
@@ -33,5 +33,35 @@ pub fn part1(dataDir: std.fs.Dir) !void {
 }
 
 pub fn part2(dataDir: std.fs.Dir) !void {
-    _ = dataDir;
+    var buffer: [14000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    const input = try read_input(dataDir, allocator, "day6.txt");
+    var prev = [_]u8{0} ** 13;
+    var freq = [_]u8{0} ** 26;
+    for (input[0..13]) |c, i| {
+        prev[12-i] = c-'a';
+        freq[c-'a'] += 1;
+    }
+
+    for(input[13..]) |c, i| {
+        freq[c-'a'] += 1;
+        var numDistinct: u8 = 0;
+        for(freq) |v| {
+            if (v == 1) {
+                numDistinct += 1;
+            }
+        }
+        if (numDistinct < 14) {
+            freq[prev[12]] -= 1;
+            var pi:u8 = 12;
+            while (pi > 0) : (pi -= 1) {
+                prev[pi] = prev[pi-1];
+            }
+            prev[0] = c - 'a';            
+        } else {
+            std.debug.print("i: {} {c}\n", .{ i + 14, c });
+            break;
+        }
+    }
 }
