bc9215870ffe5b00604c7284681485a2140a9b02
iamkroot <kroot.patel@gmail.com>
2023-02-11 19:31:31 +0530

    day21: Part1

--- src/day21.zig
+++ src/day21.zig
@@ -26,14 +26,32 @@ const CalcTy = enum {
             .div => '/',
         };
     }
+    fn interp(self: CalcTy, val1: i64, val2: i64) i64 {
+        return switch (self) {
+            .direct => val1,
+            .add => val1 + val2,
+            .mul => val1 * val2,
+            .sub => val1 - val2,
+            .div => @divExact(val1, val2),
+        };
+    }
 };
 
 const Pair = std.meta.Tuple(&[_]type{ u32, u32 });
 
 const Node = struct {
-    val: i32 = 0,
+    val: i64 = 0,
     calc_ty: CalcTy,
     deps: ?Pair = null,
+    rev_deps: std.ArrayList(u32),
+
+    fn calc(self: *Node, graph: *const Graph) void {
+        if (self.val != 0) {
+            return;
+        }
+        const ds = self.deps.?;
+        self.val = self.calc_ty.interp(graph.graph.get(ds[0]).?.val, graph.graph.get(ds[1]).?.val);
+    }
 };
 
 const StringInterner = struct {
@@ -66,6 +84,7 @@ const StringInterner = struct {
 const Graph = struct {
     interner: StringInterner,
     graph: std.AutoArrayHashMap(u32, Node),
+    rootId: u32,
 
     fn parse(allocator: std.mem.Allocator, input: []const u8) !Graph {
         var lines = std.mem.split(u8, std.mem.trim(u8, input, "\n"), "\n");
@@ -75,23 +94,36 @@ const Graph = struct {
         var graph = std.AutoArrayHashMap(u32, Node).init(allocator);
         try internTable.table.ensureTotalCapacity(n);
         try graph.ensureTotalCapacity(n);
+        var rootId: u32 = undefined;
         while (lines.next()) |line| {
             const name = line[0..4];
             const id = try internTable.insert(name);
+            if (std.mem.eql(u8, name, "root")) {
+                rootId = id;
+            }
             var node: Node = undefined;
             if (std.ascii.isDigit(line[6])) {
                 var d: usize = 0;
                 const val = parseIntChomp(line[6..], &d);
-                node = Node{ .val = val, .calc_ty = .direct };
+                node = Node{ .val = val, .calc_ty = .direct, .rev_deps = std.ArrayList(u32).init(allocator) };
             } else {
                 const calc_ty = CalcTy.fromOpCh(line[11]);
                 const firstDep = try internTable.insert(line[6..10]);
                 const secondDep = try internTable.insert(line[13..17]);
-                node = Node{ .val = 0, .calc_ty = calc_ty, .deps = Pair{ firstDep, secondDep } };
+                node = Node{ .val = 0, .calc_ty = calc_ty, .deps = Pair{ firstDep, secondDep }, .rev_deps = std.ArrayList(u32).init(allocator) };
             }
             try graph.put(id, node);
         }
-        return Graph{ .graph = graph, .interner = internTable };
+
+        // populate rev_deps
+        var iter = graph.iterator();
+        while (iter.next()) |e| {
+            if (e.value_ptr.deps) |deps| {
+                try graph.getPtr(deps[0]).?.rev_deps.append(e.key_ptr.*);
+                try graph.getPtr(deps[1]).?.rev_deps.append(e.key_ptr.*);
+            }
+        }
+        return Graph{ .graph = graph, .interner = internTable, .rootId = rootId };
     }
 
     fn print(self: *const Graph) void {
@@ -108,14 +140,97 @@ const Graph = struct {
     }
 };
 
+pub fn Queue(comptime Child: type) type {
+    return struct {
+        const This = @This();
+        const Node = struct {
+            data: Child,
+            next: ?*This.Node,
+        };
+        gpa: std.mem.Allocator,
+        start: ?*This.Node,
+        end: ?*This.Node,
+
+        pub fn init(gpa: std.mem.Allocator) This {
+            return This{
+                .gpa = gpa,
+                .start = null,
+                .end = null,
+            };
+        }
+        pub fn enqueue(this: *This, value: Child) !void {
+            const node = try this.gpa.create(This.Node);
+            node.* = .{ .data = value, .next = null };
+            if (this.end) |end| end.next = node //
+            else this.start = node;
+            this.end = node;
+        }
+        pub fn dequeue(this: *This) ?Child {
+            const start = this.start orelse return null;
+            defer this.gpa.destroy(start);
+            if (start.next) |next|
+                this.start = next
+            else {
+                this.start = null;
+                this.end = null;
+            }
+            return start.data;
+        }
+        pub fn format(
+            self: Queue(Child),
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) !void {
+            _ = fmt;
+            _ = options;
+            var start = self.start orelse null;
+            try writer.writeAll("{ ");
+            while (start) |node| {
+                try writer.print("{} ", .{node.data});
+                start = node.next;
+            }
+            try writer.writeAll("}");
+        }
+    };
+}
+
+fn toposort(graph: *Graph, allocator: std.mem.Allocator) !void {
+
+    var q = Queue(u32).init(allocator);
+    {
+        var iter = graph.graph.iterator();
+        while (iter.next()) |e| {
+            if (e.value_ptr.calc_ty == .direct) {
+                try q.enqueue(e.key_ptr.*);
+            }
+        }
+    }
+    while (q.dequeue()) |n| {
+        const nodeId = n;
+        const node = graph.graph.getPtr(nodeId).?;
+        for (node.rev_deps.items) |childId| {
+            const childNode = graph.graph.getPtr(childId).?;
+            const otherDepId: u32 = if (childNode.deps.?[0] == nodeId) childNode.deps.?[1] else childNode.deps.?[0];
+            const otherDepNode = graph.graph.getPtr(otherDepId).?;
+            if (otherDepNode.val != 0) {
+                childNode.calc(graph);
+                try q.enqueue(childId);
+            }
+        }
+    }
+    std.debug.print("root {}\n", .{graph.graph.get(graph.rootId).?.val});
+}
+
 pub fn part1(dataDir: std.fs.Dir) !void {
-    var buffer: [14000]u8 = undefined;
+    var buffer: [1400000]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
     const allocator = fba.allocator();
-    const input = try read_input(dataDir, allocator, "day21_dummy.txt");
+    const input = try read_input(dataDir, allocator, "day21.txt");
     defer allocator.free(input);
     var graph = try Graph.parse(allocator, input);
     graph.print();
+    try toposort(&graph, allocator);
 }
 
 pub fn part2(dataDir: std.fs.Dir) !void {
