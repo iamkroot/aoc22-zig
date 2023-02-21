e45ac8fa34efef7c5a809fb026c00001fd878628
iamkroot <kroot.patel@gmail.com>
2023-02-21 22:33:00 +0530

    day22: Part1

--- src/day22.zig
+++ src/day22.zig
@@ -21,35 +21,34 @@ const Idx = struct {
     }
 };
 
-const Cell = enum {
+const Cell = enum(u8) {
+    const Self = @This();
     /// Cannot land on it
-    vacuum,
+    vacuum = ' ',
     /// empty cell
-    empty,
+    empty = '.',
     /// Obstacle
-    rock,
+    rock = '#',
+    left = '<',
+    right = '>',
+    up = '^',
+    down = 'v',
 
-    fn fromCh(ch: u8) Cell {
-        return switch (ch) {
-            ' ' => .vacuum,
-            '.' => .empty,
-            '#' => .rock,
-            else => unreachable,
-        };
+    fn fromCh(ch: u8) Self {
+        return @intToEnum(Self, ch);
     }
     pub fn format(
-        self: Cell,
+        self: Self,
         comptime fmt: []const u8,
         options: std.fmt.FormatOptions,
         writer: anytype,
     ) !void {
         _ = fmt;
         _ = options;
-        try writer.print("{c}", .{switch (self) {
-            .vacuum => @as(u8, ' '),
-            .empty => '.',
-            .rock => '#',
-        }});
+        try writer.print("{c}", .{@enumToInt(self)});
+    }
+    fn facing(self: Self) Facing {
+        return @intToEnum(Facing, @enumToInt(self));
     }
 };
 
@@ -72,8 +71,10 @@ pub fn Grid(comptime T: type, comptime fmtEl: []const u8) type {
                 if (c != '\n') {
                     const cell = Cell.fromCh(c);
                     nums[i * numCols + j] = cell;
-                    if (start == null and cell != .vacuum) {
+                    if (start == null and cell == .empty) {
                         start = Idx{ .i = i, .j = j };
+                        // we will implicitly do a cw turn at the start
+                        nums[i * numCols + j] = Cell.up;
                     }
                     j += 1;
                 } else {
@@ -89,20 +90,6 @@ pub fn Grid(comptime T: type, comptime fmtEl: []const u8) type {
                 .numRows = numRows,
                 .nums = nums,
                 .start = start.?,
-                // .end = end,
-                .allocator = allocator,
-            };
-        }
-
-        fn initSingle(allocator: std.mem.Allocator, mainGrid: anytype, val: T) !Self {
-            var nums = try allocator.alloc(T, mainGrid.numRows * mainGrid.numCols);
-            std.mem.set(T, nums, val);
-            return Self{
-                .numRows = mainGrid.numRows,
-                .numCols = mainGrid.numCols,
-                .start = mainGrid.start,
-                .end = mainGrid.end,
-                .nums = nums,
                 .allocator = allocator,
             };
         }
@@ -142,72 +129,135 @@ pub fn Grid(comptime T: type, comptime fmtEl: []const u8) type {
                 }
             }
         }
+
+        fn getNext(self: *Self, facing: Facing, idx: Idx) Idx {
+            var i = idx.i;
+            var j = idx.j;
+            var newi = i;
+            var newj = j;
+            switch (facing) {
+                .down => newi = if (i + 1 == self.numRows) 0 else i + 1,
+                .up => newi = if (i == 0) self.numRows - 1 else i - 1,
+                .right => newj = if (j + 1 == self.numCols) 0 else j + 1,
+                .left => newj = if (j == 0) self.numCols - 1 else j - 1,
+            }
+            return Idx{ .i = newi, .j = newj };
+        }
+
+        fn getNext2(self: *Self, facing: Facing) Idx {
+            if (T == Cell) {
+                var nextIdx = self.getNext(facing, self.start);
+                var nextVal = self.getvalIdx(nextIdx);
+                while (nextVal == .vacuum) {
+                    nextIdx = self.getNext(facing, nextIdx);
+                    nextVal = self.getvalIdx(nextIdx);
+                }
+                return nextIdx;
+            }
+        }
+
+        fn makeMove(self: *Self, move: Move) void {
+            var amt: i32 = @intCast(i32, move.forward);
+            var curCell = self.getIdx(self.start);
+            // std.debug.print("start : {any} {any}\n", .{ self.start, curCell.* });
+
+            var curFacing = @intToEnum(Facing, @enumToInt(curCell.*));
+            const facing = curFacing.makeTurn(move.dir);
+            // std.debug.print("facings: {any} {any}\n", .{ curFacing, facing });
+
+            curCell.* = @intToEnum(T, @enumToInt(facing));
+            var i: i32 = 0;
+            while (i < amt) : (i += 1) {
+                const nextIdx = self.getNext2(facing);
+                var nextCell = self.getIdx(nextIdx);
+                // std.debug.print("step : {any} {any}\n", .{ nextIdx, nextCell.* });
+
+                if (nextCell.* == .rock) {
+                    break;
+                }
+                self.start.i = nextIdx.i;
+                self.start.j = nextIdx.j;
+                nextCell.* = @intToEnum(T, @enumToInt(facing));
+            }
+            // std.debug.print("final idx: {any}\n", .{ self.start });
+        }
     };
 }
 
 const InputGrid = Grid(Cell, "{any}");
 
-const Turn = enum {
-    cw,
-    ccw,
-    fn fromCh(ch: u8) Turn {
-        return switch (ch) {
-            'R' => .cw,
-            'L' => .ccw,
-            else => unreachable,
-        };
+const Turn = enum(u8) {
+    const Self = @This();
+    cw = 'R',
+    ccw = 'L',
+
+    fn fromCh(ch: u8) Self {
+        return @intToEnum(Self, ch);
     }
     pub fn format(
-        self: Turn,
+        self: Self,
         comptime fmt: []const u8,
         options: std.fmt.FormatOptions,
         writer: anytype,
     ) !void {
         _ = fmt;
         _ = options;
-        try writer.print("{c}", .{switch (self) {
-            .cw => @as(u8, 'R'),
-            .ccw => 'L',
-        }});
+        try writer.print("{c}", .{@enumToInt(self)});
     }
 };
 
-const Facing = enum {
-    left,
-    right,
-    up,
-    down,
-    fn fromCh(ch: u8) Facing {
-        return switch (ch) {
-            '<' => .left,
-            '>' => .right,
-            '^' => .up,
-            'v' => .down,
-            else => unreachable,
-        };
+const Facing = enum(u8) {
+    const Self = @This();
+    left = '<',
+    right = '>',
+    up = '^',
+    down = 'v',
+
+    fn fromCh(ch: u8) Self {
+        return @intToEnum(Self, ch);
     }
     pub fn format(
-        self: Facing,
+        self: Self,
         comptime fmt: []const u8,
         options: std.fmt.FormatOptions,
         writer: anytype,
     ) !void {
         _ = fmt;
         _ = options;
-        try writer.print("{c}", .{switch (self) {
-            .left => '<',
-            .right => '>',
-            .up => '^',
-            .down => 'v',
-        }});
+        try writer.print("{c}", .{@enumToInt(self)});
+    }
+    fn makeTurn(self: Self, turn: Turn) Self {
+        return switch (turn) {
+            .cw => switch (self) {
+                .down => @as(Self, .left),
+                .up => .right,
+                .left => .up,
+                .right => .down,
+            },
+            .ccw => switch (self) {
+                .up => .left,
+                .down => .right,
+                .right => .up,
+                .left => .down,
+            },
+        };
+    }
+    fn score(self: Self) u32 {
+        return switch (self) {
+            .right => 0,
+            .down => 1,
+            .left => 2,
+            .up => 3,
+        };
     }
 };
 
 const MoveT = enum { forward, turn };
 
-const Move = union(MoveT) {
+const Move = struct {
+    const Self = @This();
     forward: u32,
-    turn: Turn,
+    dir: Turn,
     fn parse(allocator: Allocator, input: []const u8) ![]Move {
         var d: usize = 0;
         // first count the number of moves
@@ -220,16 +270,15 @@ const Move = union(MoveT) {
             }
             d += 1;
         }
-        var moves = try allocator.alloc(Move, 2 * n - 1);
+        var moves = try allocator.alloc(Move, n);
         var i: usize = 0;
         d = 0;
-        moves[i] = Move{ .forward = @intCast(u32, parseIntChomp(input, &d)) };
+        moves[0] = Move{ .dir = .cw, .forward = @intCast(u32, parseIntChomp(input, &d)) };
         i += 1;
-        while (i < 2 * n - 1) {
-            moves[i] = Move{ .turn = Turn.fromCh(input[d]) };
+        while (i < n) : (i += 1) {
+            const turn = Turn.fromCh(input[d]);
             d += 1;
-            moves[i + 1] = Move{ .forward = @intCast(u32, parseIntChomp(input, &d)) };
-            i += 2;
+            moves[i] = Move{ .dir = turn, .forward = @intCast(u32, parseIntChomp(input, &d)) };
         }
         return moves;
     }
@@ -242,19 +291,11 @@ const Move = union(MoveT) {
     ) !void {
         _ = fmt;
         _ = options;
-        switch (self) {
-            .forward => |val| try writer.print("{d}", .{val}),
-            .turn => |val| try writer.print("{any}", .{val}),
-        }
+        try writer.print("{c}{d}", .{ self.dir, self.forward });
     }
 };
 
-pub fn part1(dataDir: std.fs.Dir) !void {
-    var buffer: [14000]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    const allocator = fba.allocator();
-    const input = try read_input(dataDir, allocator, "day22_dummy.txt");
-    defer allocator.free(input);
+fn parseInput(allocator: Allocator, input: []const u8) !std.meta.Tuple(&[_]type{ InputGrid, []const Move }) {
     var parts = std.mem.split(u8, std.mem.trim(u8, input, "\n"), "\n\n");
     var gridInp = parts.next().?;
     var numRows = std.mem.count(u8, std.mem.trim(u8, gridInp, "\n"), "\n") + 1;
@@ -266,12 +307,28 @@ pub fn part1(dataDir: std.fs.Dir) !void {
         }
         break :brk numCols;
     };
-    std.debug.print("numCols, numRows: {} {}\n", .{ numCols, numRows });
     var grid = try InputGrid.parse(allocator, @intCast(u32, numCols), @intCast(u32, numRows), gridInp);
-    std.debug.print("grid:\n{any}\n", .{grid});
     var moveInp = parts.next().?;
     const moves = try Move.parse(allocator, moveInp);
-    std.debug.print("moves: {any}\n", .{moves});
+    return .{ grid, moves };
+}
+
+pub fn part1(dataDir: std.fs.Dir) !void {
+    var buffer: [1400000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    const input = try read_input(dataDir, allocator, "day22.txt");
+    defer allocator.free(input);
+    var inp = try parseInput(allocator, input);
+    var grid = inp[0];
+    var moves = inp[1];
+    // std.debug.print("moves: {any}\n", .{moves});
+    for (moves) |move| {
+        grid.makeMove(move);
+        // std.debug.print("grid:\n{any}\n", .{grid});
+    } 
+    const score = (grid.start.i + 1) * 1000 + (grid.start.j + 1) * 4 + grid.getvalIdx(grid.start).facing().score();
+    std.debug.print("score: {}\n", .{ score });
 }
 
 pub fn part2(dataDir: std.fs.Dir) !void {
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
-const day21 = @import("day21.zig");
+const day22 = @import("day22.zig");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const dataDirPath = try std.fs.realpathAlloc(allocator, "data");
     const dataDir = try std.fs.openDirAbsolute(dataDirPath, .{});
-    try day21.part1(dataDir);
-    try day21.part2(dataDir);
+    try day22.part1(dataDir);
+    try day22.part2(dataDir);
 }
